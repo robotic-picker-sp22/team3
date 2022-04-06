069077be63f8e8fcd642c3803a0877d4165a9e17
Noah Ponto <ponton@uw.edu>
2022-04-06 16:40:27 -0700

    lab 2 finished

--- robot_api/src/robot_api/base.py
+++ robot_api/src/robot_api/base.py
@@ -1,8 +1,21 @@
 #! /usr/bin/env python
 
-# TODO: import ????????_msgs.msg
+from geometry_msgs.msg import Twist
 import rospy
 
+'''
+Useful Refrence Documents:
+
+Creating a simple publisher: 
+https://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
+
+Creating a custom publisher:
+http://wiki.ros.org/ROS/Tutorials/CustomMessagePublisherSubscriber%28python%29
+'''
+
+# Constants
+TOPIC = 'cmd_vel'
+NODE_NAME = 'base'
 
 class Base(object):
     """Base controls the mobile base portion of the Fetch robot.
@@ -13,9 +26,13 @@ class Base(object):
             base.move(0.2, 0)
         base.stop()
     """
+    _publisher: rospy.Publisher
 
     def __init__(self):
         # TODO: Create publisher
+        self._publisher = rospy.Publisher(TOPIC, Twist, queue_size=10)
+        # rospy.init_node(NODE_NAME, anonymous=True)
+        # NOTE: not worried abt rate bc this is handled by teleop app
         pass
 
     def move(self, linear_speed, angular_speed):
@@ -31,12 +48,20 @@ class Base(object):
                 value means the robot should rotate clockwise.
         """
         # TODO: Create Twist msg
+        msg = Twist()
         # TODO: Fill out msg
+        msg.linear.x = linear_speed
+        msg.angular.z = angular_speed
         # TODO: Publish msg
-        rospy.logerr('Not implemented.')
+        self._publisher.publish(msg)
+        # rospy.loginfo(msg)
 
     def stop(self):
         """Stops the mobile base from moving.
         """
         # TODO: Publish 0 velocity
+        msg = Twist()
+        msg.linear.x = 0
+        msg.angular.z = 0
+
         rospy.logerr('Not implemented.')
