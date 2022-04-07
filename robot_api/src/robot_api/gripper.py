e62a0cf1188034d855a391a6d113e74cd2f3957c
lab computer <capstone@dujour.cs.washington.edu>
2022-04-07 13:40:47 -0700

    lab 3 finished

--- robot_api/src/robot_api/gripper.py
+++ robot_api/src/robot_api/gripper.py
@@ -19,16 +19,21 @@ class Gripper(object):
 
     def __init__(self):
         # TODO: Create actionlib client
+        self._client = actionlib.SimpleActionClient('/gripper_controller/gripper_action', control_msgs.msg.GripperCommandAction)
         # TODO: Wait for server
-        pass
+        self._client.wait_for_server()
 
     def open(self):
         """Opens the gripper.
         """
         # TODO: Create goal
+        goal = control_msgs.msg.GripperCommandGoal()
+        goal.command.position = OPENED_POS
+        goal.command.max_effort = self.MIN_EFFORT
         # TODO: Send goal
+        self._client.send_goal(goal)
         # TODO: Wait for result
-        pass
+        self._client.wait_for_result()
 
     def close(self, max_effort=MAX_EFFORT):
         """Closes the gripper.
@@ -38,6 +43,10 @@ class Gripper(object):
                 should not be less than 35N, or else the gripper may not close.
         """
         # TODO: Create goal
+        goal = control_msgs.msg.GripperCommandGoal()
+        goal.command.position = CLOSED_POS
+        goal.command.max_effort = self.MAX_EFFORT
         # TODO: Send goal
+        self._client.send_goal(goal)
         # TODO: Wait for result
-        pass
+        self._client.wait_for_result()
