d7db1054cd8161a50729c34f94c6d46c7d8ba06c
lab computer <capstone@dujour.cs.washington.edu>
2022-04-08 12:05:08 -0700

    finished lab 7

--- robot_api/src/robot_api/__init__.py
+++ robot_api/src/robot_api/__init__.py
@@ -3,3 +3,6 @@ from .gripper import Gripper
 from .torso import Torso
 # Lab6: import Head class into robot_api module
 from .head import Head
+# Lab7
+from .arm import Arm
+from .arm_joints import ArmJoints
--- robot_api/src/robot_api/arm.py
+++ robot_api/src/robot_api/arm.py
@@ -1,9 +1,15 @@
 # TODO: import ?????????
 # TODO: import ???????_msgs.msg
 # TODO: import ??????????_msgs.msg
+import actionlib
+import control_msgs.msg
+import trajectory_msgs.msg
 import rospy
 
 from .arm_joints import ArmJoints
+
+ACTION_NAME = 'arm_controller/follow_joint_trajectory'
+TIME_FROM_START = 5
 
 
 class Arm(object):
@@ -18,10 +24,12 @@ class Arm(object):
 
     def __init__(self):
         # TODO: Create actionlib client
+        self._client = actionlib.SimpleActionClient(ACTION_NAME, control_msgs.msg.FollowJointTrajectoryAction)
         # TODO: Wait for server
+        self._client.wait_for_server()
         pass
 
-    def move_to_joints(self, arm_joints):
+    def move_to_joints(self, arm_joints: ArmJoints):
         """Moves the robot's arm to the given joints.
 
         Args:
@@ -29,13 +37,21 @@ class Arm(object):
                 the arm.
         """
         # TODO: Create a trajectory point
+        point = trajectory_msgs.msg.JointTrajectoryPoint()
         # TODO: Set position of trajectory point
+        point.positions = arm_joints.values()
         # TODO: Set time of trajectory point
+        point.time_from_start = rospy.Time(TIME_FROM_START)
 
         # TODO: Create goal
+        goal = control_msgs.msg.FollowJointTrajectoryGoal()
         # TODO: Add joint name to list
+        goal.trajectory.joint_names = arm_joints.names()
         # TODO: Add the trajectory point created above to trajectory
+        goal.trajectory.points = [point]
 
         # TODO: Send goal
+        self._client.send_goal(goal)
         # TODO: Wait for result
-        rospy.logerr('Not implemented.')
+        self._client.wait_for_result()
+        rospy.loginfo(f'Done moving arm {list(zip(arm_joints.names(), arm_joints.values()))}')
