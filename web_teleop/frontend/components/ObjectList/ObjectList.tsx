db5e962481d3cb4bfdfcdf6b75753f7ad9aeb94a
lab computer <capstone@dujour.cs.washington.edu>
2022-05-31 12:36:36 -0700

    frontend fix form error

--- web_teleop/frontend/components/ObjectList/ObjectList.tsx
+++ web_teleop/frontend/components/ObjectList/ObjectList.tsx
@@ -3,6 +3,7 @@ import ROSLIB from "roslib"
 import { Autocomplete } from '@mantine/core';
 import { objectList } from '../../utils/constants'
 import ObjectListItem from "../ObjectListItem/ObjectListItem";
+import { formList, useForm } from "@mantine/form";
 
 type ObjectListProps = {
     ros: ROSLIB.Ros
@@ -11,33 +12,23 @@ const SERVICE_NAME = "/web_teleop/set_object_list"
 const SERVICE_TYPE = "/web_teleop/SetObjectList"
 
 export default function ObjectList({ ros }: ObjectListProps) {
-    const [objects, setObjects] = useState<string[]>([])
     const [inputText, setInputText] = useState<string>("")
+    const form = useForm({
+        initialValues: {
+            objects: formList<string>([])
+        }
+    })
 
     const service = new ROSLIB.Service({
         ros,
         name: SERVICE_NAME,
         serviceType: SERVICE_TYPE,
     })
-
-    const createDeleteCallback = (i: number) => {
-        return (objectName: string) => {
-            setObjects(oldList => {
-                if (oldList[i] == objectName) {
-                    return oldList.splice(i, 1)
-                } else {
-                    console.error(`Could not remove item: ${objectName} at index ${i}`)
-                    return oldList
-                }
-            }
-            )
-        }
-    }
     return (
         <div>
             <ul>
-                {objects.map((object, i) => (
-                    <ObjectListItem key={`${object}-${i}`} object={object} deleteCallback={createDeleteCallback(i)}/>
+                {form.values.objects.map((object, i) => (
+                    <ObjectListItem key={`${object}-${i}`} object={object} deleteCallback={() => form.removeListItem('objects', i)}/>
                 ))}
             </ul>
             <input type="text" onChange={e => {
@@ -53,21 +44,16 @@ export default function ObjectList({ ros }: ObjectListProps) {
             />
             <button onClick={() => {
                 if (objectList.includes(inputText)) {
-                    setObjects(old => {
-                        return [...old, inputText]
-                    })
-                    setInputText("")
+                    form.addListItem("objects", inputText)
                 }
             }}>Add Item</button>
             <button onClick={() => {
-
-                const request = new ROSLIB.ServiceRequest({ objects })
+                const request = new ROSLIB.ServiceRequest({ objects: form.values.objects })
                 service.callService(request, (e) => {
                     console.log("Received Response", e);
                 },
                 (e) => {
                     console.log("Failed", e);
-                    
                 })
                 
             }}>Send List</button>
