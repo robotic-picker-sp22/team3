4558627e99a82ed39d65ea57bc876f28ebd100b1
lab computer <capstone@dujour.cs.washington.edu>
2022-06-01 12:41:23 -0700

    added bag files for objects

--- extract_features.sh
+++ extract_features.sh
@@ -1,12 +1,18 @@
 . /opt/ros/noetic/setup.bash
 . ~/catkin_ws/devel/setup.bash
 
-rm -rf combined_labels
-mkdir combined_labels
-rosrun perception extract_features pill_bottle.bag  pill_bottle
-rosrun perception extract_features pill_box.bag     pill_box
-rosrun perception extract_features sink_basket.bag  sink_basket
-rosrun perception extract_features box.bag          box
-rosrun perception extract_features tape_measure.bag tape_measure
+rm -rf final_labels
+mkdir final_labels
+shopt -s nullglob
 
-mv *_label.bag combined_labels
+for file in raw_item_bags/*
+do
+    rosrun perception extract_features $file ${file%.*}
+done
+# rosrun perception extract_features pill_bottle.bag  pill_bottle
+# rosrun perception extract_features pill_box.bag     pill_box
+# rosrun perception extract_features sink_basket.bag  sink_basket
+# rosrun perception extract_features box.bag          box
+# rosrun perception extract_features tape_measure.bag tape_measure
+shopt -u nullglob
+mv *_label.bag final_labels
